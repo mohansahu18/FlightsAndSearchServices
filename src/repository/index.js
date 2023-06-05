63bc93bc5fc5ae05c1ae78fcff109b14c6af982e
mohan lal sahu <mohansahu18111811@gmail.com>
2023-06-05 13:53:43 +0530

    added city services

---
+++ src/repository/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    CityRipository: require("./city-repository")
+}
---
+++ src/services/city-services.js
@@ -0,0 +1,48 @@
+const { CityRipository } = require('../repository/index');
+
+class CityServices {
+    constructor() {
+        this.cityRipository = new CityRipository()
+    }
+
+    async createCity(data) {
+        try {
+            const city = await this.CityRipository.createCity(data)
+        } catch (error) {
+            console.log("something went wrong in services layer")
+            throw { error }
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const response = await this.cityRipository.deleteCity(cityId);
+            return response
+        } catch (error) {
+            console.log("something went wrong in services layer")
+            throw { error }
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityRipository.updateCity(cityId, data);
+            return city
+        } catch (error) {
+            console.log("something went wrong in services layer")
+            throw { error }
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.cityRipository.getCity(cityId)
+            return city
+
+        } catch (error) {
+            console.log("something went wrong in services layer")
+            throw { error }
+        }
+    }
+
+}
